772183a9365e86255370ddf8b7c8d025ef56952f
Ehsan Rahimi <57280632+ehsanrah@users.noreply.github.com>
2022-02-10 16:50:29 -0500

    Update SharedWallet.sol

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,31 +1,21 @@
 pragma solidity ^0.8.0;
 //SPDX-License-Identifier: UNLICENSED
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-
-contract allowance is Ownable {
-    mapping(address => uint) public allowance;
-    function addAllowence(address _addr, uint _amount) external onlyOwner() {
-        allowance[_addr] = _amount;
-    }
-    modifier OwnerOrAllowed (uint _amount) {
-        require(owner() == msg.sender|| allowance[msg.sender] >= _amount,"You are not allowed!" );
-        _;
-    }
-    function reduceAllowance(address _who, uint _amount) internal{
-        allowance[_who] -= _amount;
-    }
-}
-contract SharedWallet is allowance {
-    
+import "./Allowance.sol";
+contract SimpleWallet is allowance { 
+    event MoneySent(address indexed _beneficiary, uint _amount);    
+    event MoneyRecieved (address _from, uint _amount);
     function withdrawMoney(address payable _to, uint _amount) public OwnerOrAllowed(_amount) {
+        emit MoneySent(_to, _amount);
         require(_amount <= address(this).balance, "There is not enough funds stored in the smart contract");
         _to.transfer(_amount);
         if(owner() != msg.sender){
             reduceAllowance(msg.sender, _amount);
-        }
-        
+        }   
+    }
+    function renounceOwnership() public override onlyOwner() {
+        revert("You can't do this!");
     }
     fallback () external payable {
-
+        emit MoneyRecieved(msg.sender, msg.value);
     }
 }
